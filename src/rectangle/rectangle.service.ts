ec53711538265d4868068e6af0849e6ea9169fe5
Your Name <you@example.com>
2024-04-19 18:13:53 +0330

    fix : find similar rectangle query fixed

--- src/rectangle/rectangle.service.ts
+++ src/rectangle/rectangle.service.ts
@@ -13,26 +13,58 @@ export class RectangleService {
     ) { }
 
     async saveRectangle(body: RectangleInputDto): Promise<IRectangle[]> {
-        const { input, main } = body
-        let rectangles: IRectangle[] = []
-        for (const rectangle of input) {
-            if (
-                (rectangle.x === main.x)
-                ||
-                (rectangle.y === main.y)
-                ||
-                (rectangle.height === main.height)
-                ||
-                (rectangle.width === main.width)
-            ) {
-                rectangles.push(rectangle)
-                await this.rectangleRepository.save(rectangle);
+        try {
+            const { input, main } = body
+            let rectangles: IRectangle[] = []
+            for (const rectangle of input) {
+                if (this.checkCommonRectangles(main, rectangle)) {
+                    rectangles.push(rectangle)
+                    await this.rectangleRepository.save(rectangle);
+                }
             }
+            return rectangles;
         }
-        return rectangles;
+        catch (err) {
+            throw err
+        }
     }
 
     async getAllRectangles(): Promise<Rectangle[]> {
-        return this.rectangleRepository.find();
+        try {
+            return this.rectangleRepository.find();
+        } catch (err) {
+            throw err
+        }
+    }
+
+    private checkCommonRectangles(rect1: IRectangle, rect2: IRectangle): Boolean {
+        try {
+            let xOverlap = Math.max(0, Math.min(rect1.x + rect1.width, rect2.x + rect2.width) - Math.max(rect1.x, rect2.x));
+            let yOverlap = Math.max(0, Math.min(rect1.y + rect1.height, rect2.y + rect2.height) - Math.max(rect1.y, rect2.y));
+            if (
+                //Rectangles touch but do not overlap
+                ((xOverlap > 0 || yOverlap > 0) && ((xOverlap === 0 && yOverlap > 0) || (xOverlap > 0 && yOverlap === 0)))
+                ||
+                //"Rectangles overlap 
+                (xOverlap > 0 && yOverlap > 0)
+                ||
+                //Rectangles intersect at corners
+                (
+                    (xOverlap === 0 && yOverlap === 0) &&
+                    (
+                        (rect1.x === rect2.x + rect2.width && rect1.y === rect2.y + rect2.height) ||
+                        (rect1.x === rect2.x + rect2.width && rect1.y + rect1.height === rect2.y) ||
+                        (rect1.x + rect1.width === rect2.x && rect1.y === rect2.y + rect2.height) ||
+                        (rect1.x + rect1.width === rect2.x && rect1.y + rect1.height === rect2.y)
+                    )
+                )
+            ) {
+                return true
+            } else {
+                return false
+            }
+        } catch (err) {
+            throw err
+        }
     }
 }
