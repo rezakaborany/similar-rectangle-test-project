94993f18488879bdf69c2937ee48e1024b7ceb8e
Your Name <you@example.com>
2024-04-19 19:04:56 +0330

    fix : find similar rectangle query fixed

--- src/rectangle/rectangle.service.ts
+++ src/rectangle/rectangle.service.ts
@@ -38,33 +38,20 @@ export class RectangleService {
     }
 
     private checkCommonRectangles(rect1: IRectangle, rect2: IRectangle): Boolean {
-        try {
-            let xOverlap = Math.max(0, Math.min(rect1.x + rect1.width, rect2.x + rect2.width) - Math.max(rect1.x, rect2.x));
-            let yOverlap = Math.max(0, Math.min(rect1.y + rect1.height, rect2.y + rect2.height) - Math.max(rect1.y, rect2.y));
-            if (
-                //Rectangles touch but do not overlap
-                ((xOverlap > 0 || yOverlap > 0) && ((xOverlap === 0 && yOverlap > 0) || (xOverlap > 0 && yOverlap === 0)))
-                ||
-                //"Rectangles overlap 
-                (xOverlap > 0 && yOverlap > 0)
-                ||
-                //Rectangles intersect at corners
-                (
-                    (xOverlap === 0 && yOverlap === 0) &&
-                    (
-                        (rect1.x === rect2.x + rect2.width && rect1.y === rect2.y + rect2.height) ||
-                        (rect1.x === rect2.x + rect2.width && rect1.y + rect1.height === rect2.y) ||
-                        (rect1.x + rect1.width === rect2.x && rect1.y === rect2.y + rect2.height) ||
-                        (rect1.x + rect1.width === rect2.x && rect1.y + rect1.height === rect2.y)
-                    )
-                )
-            ) {
-                return true
-            } else {
-                return false
-            }
-        } catch (err) {
-            throw err
-        }
+        const xOverlap = Math.max(0, Math.min(rect1.x + rect1.width, rect2.x + rect2.width) - Math.max(rect1.x, rect2.x));
+        const yOverlap = Math.max(0, Math.min(rect1.y + rect1.height, rect2.y + rect2.height) - Math.max(rect1.y, rect2.y));
+
+        const touchingButNotOverlapping = (xOverlap === 0 && yOverlap > 0) || (xOverlap > 0 && yOverlap === 0);
+        const overlapping = xOverlap > 0 && yOverlap > 0;
+        const intersectingAtCorners =
+            (xOverlap === 0 && yOverlap === 0) &&
+            (
+                (rect1.x === rect2.x + rect2.width && rect1.y === rect2.y + rect2.height) ||
+                (rect1.x === rect2.x + rect2.width && rect1.y + rect1.height === rect2.y) ||
+                (rect1.x + rect1.width === rect2.x && rect1.y === rect2.y + rect2.height) ||
+                (rect1.x + rect1.width === rect2.x && rect1.y + rect1.height === rect2.y)
+            );
+
+        return touchingButNotOverlapping || overlapping || intersectingAtCorners;
     }
 }
